3a3234a6c51ae1866b69186703c8c8a5c63c52f3
liiir1985 <liiir1985@users.noreply.github.com>
2019-09-27 17:28:33 +0800

    added sample project

---
+++ Playground/X86ToLLVM/Executables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using AsmResolver;
+namespace X86ToLLVM
+{
+    public class Executables
+    {
+        WindowsAssembly asm;
+        public void Load(string path)
+        {
+            asm = WindowsAssembly.FromFile(path);
+            
+        }
+    }
+}
